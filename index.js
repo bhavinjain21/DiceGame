cd6962e59b04707400c4f520ffbab44c9712257c
bhavinjain21 <139436743+bhavinjain21@users.noreply.github.com>
2023-07-13 21:33:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,41 +2,41 @@ var a = Math.floor((Math.random()*6)+1);
 var b = Math.floor((Math.random()*6)+1);
 
 if(a===1){
-  document.querySelector(".img1").setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHINJPyxf_0uLTIExoOxFWNi7c15BkLz3k2o5htMFjEX7yjw5pIIlcBNuA&s=10");
+  document.querySelector(".img1").setAttribute("src","d1.png");
   }
 else if(a===2){
-  document.querySelector(".img1").setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeDzXWVnwqFR5-woHoZksmrDRZyytw4JkqNrh5s7vai3ja9QeZ8WqzTa4&s=10");
+  document.querySelector(".img1").setAttribute("src","d2.png");
 }
 else if(a===3){
-  document.querySelector(".img1").setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF0hsA3JBzTYfRIPEkm-mVN2NOrmHlSfawQ74pAb_myQaB0v7eGkF6dtA&s=10");
+  document.querySelector(".img1").setAttribute("src","d3.png");
 }
 else if(a===4){
-  document.querySelector(".img1").setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXDXfhT9IE-wV7xAoncTOnEaVfOxRnd-Y6z0Vf6RzV7Rzj6h_F18ahQ6E&s=10");
+  document.querySelector(".img1").setAttribute("src","d4.png");
 }
 else if(a===5){
-  document.querySelector(".img1").setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ197VTVapfVbYCsEtJ6ceuEZFujZfR6rl_GQ&usqp=CAU");
+  document.querySelector(".img1").setAttribute("src","d5.png");
 }
 else{
-  document.querySelector(".img1").setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcnpPtntN7j4QygBwXmhSd_XGk2xmGY77wzQ&usqp=CAU");
+  document.querySelector(".img1").setAttribute("src","d6.png");
 }
 
 if(b===1){
-  document.querySelector(".img2").setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHINJPyxf_0uLTIExoOxFWNi7c15BkLz3k2o5htMFjEX7yjw5pIIlcBNuA&s=10");
+  document.querySelector(".img2").setAttribute("src","d1.png");
   }
 else if(b===2){
-  document.querySelector(".img2").setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeDzXWVnwqFR5-woHoZksmrDRZyytw4JkqNrh5s7vai3ja9QeZ8WqzTa4&s=10");
+  document.querySelector(".img2").setAttribute("src","d2.png");
 }
 else if(b===3){
-  document.querySelector(".img2").setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF0hsA3JBzTYfRIPEkm-mVN2NOrmHlSfawQ74pAb_myQaB0v7eGkF6dtA&s=10");
+  document.querySelector(".img2").setAttribute("src","d3.png");
 }
 else if(b===4){
-  document.querySelector(".img2").setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXDXfhT9IE-wV7xAoncTOnEaVfOxRnd-Y6z0Vf6RzV7Rzj6h_F18ahQ6E&s=10");
+  document.querySelector(".img2").setAttribute("src","d4.png");
 }
 else if(b===5){
-  document.querySelector(".img2").setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ197VTVapfVbYCsEtJ6ceuEZFujZfR6rl_GQ&usqp=CAU");
+  document.querySelector(".img2").setAttribute("src","d5.png");
 }
 else{
-  document.querySelector(".img2").setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcnpPtntN7j4QygBwXmhSd_XGk2xmGY77wzQ&usqp=CAU");
+  document.querySelector(".img2").setAttribute("src","d6.png");
 }
 
 if(a>b){
